755538dbb46df7c9872fa4b85be427978d967635
OtavioBScar <otavio.scarpelline@gmail.com>
2024-09-16 15:00:35 -0300

    feat: interface usuario

--- src/components/CadastroForm/index.tsx
+++ src/components/CadastroForm/index.tsx
@@ -6,29 +6,36 @@ interface CadastroProps{
     setShow: () => void
 }
 
+interface usuario{
+    nome: string,
+    email: string,
+    senha: string,
+    confirmarSenha: string
+}
+
 function CadastroForm({ show, setShow }: CadastroProps) {
 
     if (!show) return null;
     return(
-        <div className="login-container">
-            <div className="login-form-container">
-                <form action="" className="login-form">
+        <div className="cadastro-container">
+            <div className="cadastro-form-container">
+                <form action="" className="cadastro-form">
                     <label htmlFor="nome">Nome completo</label>
-                    <input type="text" id="nome" name="nome" className="login-form-input" />
+                    <input type="text" id="nome" name="nome" className="cadastro-form-input" />
                     <label htmlFor="email">Email</label>
-                    <input type="text" id="email" name="email" className="login-form-input" />
+                    <input type="text" id="email" name="email" className="cadastro-form-input" />
                     <label htmlFor="senha">Senha</label>
-                    <input type="text" id="senha" name="senha" className="login-form-input" />
-                    <label htmlFor="confirmar-senha">Confirmar senha</label>
-                    <input type="text" id="confirmar-senha" name="confirmar-senha" className="login-form-input" />
+                    <input type="text" id="senha" name="senha" className="cadastro-form-input" />
+                    <label htmlFor="confirmarSenha">Confirmar senha</label>
+                    <input type="text" id="confirmar-senha" name="confirmarSenha" className="cadastro-form-input" />
                         <h4>Genero</h4>
-                    <div className="login-form-genero">
+                    <div className="cadastro-form-genero">
                         <input type="radio" id="masculino" name="genero" value="masculino" className="radio" />
                         <input type="radio" id="feminino" name="genero" value="feminino" className="radio" />
                         <input type="radio" id="none" name="genero" value="nao-informar" className="radio" />
                     </div>
                 </form>
-                <button onClick={setShow}>Já possui uma conta? Faça login</button>
+                <button onClick={setShow}>Já possui uma conta? Faça cadastro</button>
             </div>
         </div>
     )
