ff7a9a87249bfea9f7dde4e3e02bccdbd511c64c
OtavioBScar <otavio.scarpelline@gmail.com>
2024-09-16 15:01:34 -0300

    feat: required in button component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,20 +1,22 @@
 import { FC } from "react"
+import "./style.css"
 
 interface inputProps {
     label: string,
     type: string,
     name: string,
     value: string,
+    required?: boolean,
     event: (e: React.ChangeEvent<HTMLInputElement>) => void,
 }
 
-const Input = ({ label, type, name, value, event }: inputProps) => {
+const Input = ({ label, type, name, value, required, event }: inputProps) => {
 
     return (
-        <>
+        <div className="input-container">
+            <input type={type} name={name} value={value} onChange={event} required={required}/>
             <label htmlFor={name}>{label}</label>
-            <input type={type} name={name} value={value} onChange={event} />
-        </>
+        </div>
     )
 }
 
