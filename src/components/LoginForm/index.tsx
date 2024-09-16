2d395c80c793f3605cbd66b41c0f170563f71352
OtavioBScar <otavio.scarpelline@gmail.com>
2024-09-16 15:01:15 -0300

    feat: state cleaner

--- src/components/LoginForm/index.tsx
+++ src/components/LoginForm/index.tsx
@@ -14,39 +14,54 @@ function LoginForm() {
         email: '',
         senha: ''
     })
-    
+
     const [showCadastro, setShowCadastro] = useState(false);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value } = event.target
-        setUsuario((prev) => ({...prev, [name]:value}))
+        setUsuario((prev) => ({ ...prev, [name]: value }))
     }
 
-    const prevUser = {email: "otavio@mail.com", senha: "1234"}
+    const prevUser = { email: "otavio@mail.com", senha: "1234" }
 
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault()
 
-        if(prevUser.email === usuario.email && prevUser.senha === usuario.senha){
+        if (prevUser.email === usuario.email && prevUser.senha === usuario.senha) {
+            setUsuario({
+                email: '',
+                senha: ''
+            })
             return alert("Logado")
         }
         return alert("Email ou senha incorreta")
     }
-    
-    if (!!showCadastro) return <CadastroForm show={showCadastro} setShow={() => setShowCadastro(!showCadastro)}/>;
-    return(
-        <>
+
+    if (!!showCadastro) return <CadastroForm show={showCadastro} setShow={() => setShowCadastro(!showCadastro)} />;
+    return (
         <div className="login-container">
             <div className="login-form-container">
                 <form onSubmit={(e) => handleSubmit(e)} className="login-form">
-                    <Input label="Email" type="text" name="email" value={usuario.email} event={(e) => handleChange(e)}/>
-                    <Input label="Senha" type="password" name="senha" value={usuario.senha} event={(e) => handleChange(e)}/>
-                <button type="submit">Confirmar</button>
+                    <div className="input-div">
+                        <Input label="Email" type="text" name="email" value={usuario.email} required event={(e) => handleChange(e)} />
+                        <Input label="Senha" type="password" name="senha" value={usuario.senha} required event={(e) => handleChange(e)} />
+                    </div>
+                    <div className="confirm-div">
+                        <button className="confirm-button" type="submit">Confirmar</button>
+                    </div>
                 </form>
-                <button onClick={() => setShowCadastro(!showCadastro)}><p style={{fontSize:"12px", color:"blue"}}>Não possui uma conta? Cadastre-se</p></button>
+                <div className="login-with-apis">
+                    <div className="lwa-text">
+                        <p>Entrar com</p>
+                    </div>
+                    <div className="lwa-buttons">
+                        <button className="google-login">google</button>
+                        <button className="github-login">hithub</button>
+                    </div>
+                </div>
+                <button className="cadastro-button" onClick={() => setShowCadastro(!showCadastro)}><p className="cadastro-button-p">Não possui uma conta? Cadastre-se</p></button>
             </div>
         </div>
-        </>
     )
 }
 
